865636f8ca62b8aaff42e8abf4611538b0342429
Prathamesh Bhuravane <bhuravaneprathamesh@gmail.com>
2024-12-14 12:08:47 +0530

    Initial Commit

---
+++ Omnia/OmniaApp.swift
@@ -0,0 +1,33 @@
+//
+//  OmniaApp.swift
+//  Omnia
+//
+//  Created by Prathamesh Bhuravane on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct OmniaApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
